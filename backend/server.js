36533080fe88611f0ff714e29e6812e8e541a026
Akshat Dubey <iakshatdubey@gmail.com>
2024-03-27 03:00:22 +0530

    Refactor: Enhance line spacing in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
